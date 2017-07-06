7080bb92fb1fdfdb2e7cf4688e2193f22922d340
Enzo D'Onofrio <aethiss@MacBook-Pro-di-Enzo.local>
2017-07-06 19:52:04 +0100

    MeetingList dynamic update

---
+++ src/components/MeetingList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base'
+
+export default class MeetingList extends Component {
+  render() {
+    console.log(this.props.meeting)
+    const { meeting } = this.props
+    console.log(meeting.date)
+    return (
+        <Content>
+          <List>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require('../../assets/room1.jpg')} square />
+              </Left>
+              <Body>
+              <Text>Room {meeting.room} - Floor {meeting.floor} ({meeting.location})</Text>
+              <Text note>{meeting.description}</Text>
+              </Body>
+              <Right>
+                <Text note>{new Date(meeting.date).getDate()}</Text>
+              </Right>
+            </ListItem>
+          </List>
+        </Content>
+    )
+  }
+}
--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 import Meteor, { createContainer } from 'react-native-meteor'
-import { Container, Text } from 'native-base'
+import { Container, H2, Spinner } from 'native-base'
+import MeetingList from '../components/MeetingList'
 
 
 class Agenda extends Component {
   render() {
     console.log(this.props)
     return (
-    <Container>
-      <Text>Some here</Text>
-    </Container>
+      <Container>
+        <H2>My Meetings</H2>
+        { !this.props.meetingsReady && <Spinner /> }
+        { this.props.meetingsReady && this.props.meetings.map((value) => <MeetingList key={ value._id } meeting={value} />)}
+      </Container>
     )
   }
 }
 
 export default createContainer(() => {
+  const handler = Meteor.subscribe('meetings')
   return {
+    meetingsReady: handler.ready(),
     meetings: Meteor.collection('meetings').find({
       creator: Meteor.userId()
     })
