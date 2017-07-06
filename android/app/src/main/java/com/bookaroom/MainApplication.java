9922cfb9faabc7e73d04afc36d492feef31fdcd1
Callum Denby <callum.denby@arcadiagroup.co.uk>
2017-07-06 11:26:16 +0100

    Added Android firebase support

--- android/app/src/main/java/com/bookaroom/MainApplication.java
+++ android/app/src/main/java/com/bookaroom/MainApplication.java
@@ -1,5 +1,8 @@
 package com.bookaroom;
 
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseAuthPackage(),
+          new RNFirebaseDatabasePackage()
       );
     }
   };
