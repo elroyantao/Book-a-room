4d60461cc016f30cd60521de7b2a0242facc86a6
Manuel <ocanam@arcadiagroup.co.uk>
2017-07-07 10:10:05 +0100

    Add styles on Settings page

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react'
-import { Text, View, Button } from 'native-base'
-import Meteor from 'react-native-meteor'
+import { Text, Button, Body, Form, Card, CardItem } from 'native-base'
+import Meteor, { createContainer } from 'react-native-meteor'
 
-export default class Settings extends Component {
+class Settings extends Component {
   onPress = () => {
     Meteor.logout(this.props.onLogout)
   }
 
   render() {
+    const { user } = this.props
+    console.log(user)
     return (
-      <View>
-        <Text>Settings Page!</Text>
-        <Button onPress={this.onPress}>
-          <Text>Logout</Text>
+      <Form>
+        <Card>
+          <CardItem header>
+            <Text>User</Text>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text>{user.profile.first_name}</Text>
+            </Body>
+          </CardItem>
+        </Card>
+        <Card>
+          <CardItem header>
+            <Text>Email</Text>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text>{user.emails[0].address}</Text>
+            </Body>
+          </CardItem>
+        </Card>
+        <Button block primary style={style.button} onPress={this.onPress}>
+          <Text> Log out </Text>
         </Button>
-      </View>
+      </Form>
     )
+  }
+}
+
+export default createContainer(() => {
+  return {
+    user: Meteor.user()
+  }
+}, Settings)
+
+
+const style = {
+  button: {
+    marginTop: 30
   }
 }
