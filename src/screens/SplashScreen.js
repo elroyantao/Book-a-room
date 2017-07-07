d7e24fabdfc7e24f5bfbc8d98c2fed2b9942577a
Callum Denby <callum.denby@arcadiagroup.co.uk>
2017-07-07 10:09:33 +0100

    Added splashscreen spinner

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
-import { Image } from 'react-native'
+import { Image, View, StyleSheet } from 'react-native'
+import { Spinner } from 'native-base'
 
 const splashscreen = require('../../assets/splashscreen.png')
 
 export default class SplashPage extends Component {
   render() {
     return (
-      <Image
-        source={splashscreen}
-        style={{ flex: 1, height: null, width: null }}
-      />
+      <View style={styles.container}>
+        <View style={styles.backgroundContainer}>
+          <Image source={splashscreen} resizeMode='cover' style={styles.backdrop} />
+        </View>
+        <View style={styles.spinner}>
+          <Spinner />
+        </View>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  spinner: {
+    
+  },
+  backgroundContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  container: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column',
+    height: null,
+    width: null
+  }
+})
